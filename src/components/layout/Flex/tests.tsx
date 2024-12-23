096e974dafae14e1c924f0e6b8a971b85e7ddcbf
Samuil Berenzon <samus.dev.space@gmail.com>
2024-12-23 15:48:03 +0000

    added another ex test

---
+++ src/components/layout/Flex/tests.tsx
@@ -0,0 +1,8 @@
+// import { render, screen } from '@test-utils';
+import "@testing-library/jest-dom";
+
+describe("Flex", () => {
+  it("should render", () => {
+    expect(true).toBe(true);
+  });
+});
